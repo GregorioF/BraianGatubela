978c63a967879507a6426dd71799f2a0414550d5
Lucia <luciainesromero@hotmail.com>
2016-06-24 12:01:38 -0300

    Agregue lo de registro

--- registro.h
+++ registro.h
@@ -1,8 +1,8 @@
 #ifndef REGISTRO_H
 #define REGISTRO_H
 
-#include "Conj.h"
-#include "../Driver.h"
+#include "aed2/Conj.h"
+#include "Driver.cpp"
 
 using namespace aed2;
 
@@ -30,4 +30,45 @@ Conj<NombreCampo> campos(Registro& r1){
 	return res;
 }
 
+bool coincidenTodos(Registro& r1, Conj<NombreCampo>& c, Registro& r2){
+	bool res=true;
+	if(!c.EsVacio()){
+		typename::Conj<NombreCampo>::Iterador it=c.CrearIt();
+		while(it.HaySiguiente() && res==true){
+			if(r1.Significado(it.Siguiente()).tipo() != r2.Significado(it.Siguiente()).tipo()){
+				res=false;
+				}
+			else{
+				if(r1.Significado(it.Siguiente()).esNat()){
+					if(r1.Significado(it.Siguiente()).dameNat() != r2.Significado(it.Siguiente()).dameNat()){res=false;}
+					}
+				else{
+					if(r1.Significado(it.Siguiente()).dameString() != r2.Significado(it.Siguiente()).dameString()){res=false;}
+					}	
+				}
+		
+			it.Avanzar();	
+			}		
+		}
+	return res;	
+}
+
+bool borrar(Registro crit, Registro reg){
+	bool res=true;
+	Conj<NombreCampo> c=campos(crit);
+	res=coincidenTodos(crit, c, reg);
+	return res;
+}
+	
+
+void copiarCampos(Conj<NombreCampo> c, Registro& r1, Registro r2){
+	typename::Conj<NombreCampo>::Iterador it=c.CrearIt();
+	while(it.HaySiguiente()){
+		if(!r1.Definido(it.Siguiente())){
+			r1.Definir(it.Siguiente(), r2.Significado(it.Siguiente()));
+			}
+		it.Avanzar();	
+		} 
+	}
+	
 #endif
